fb9966e69aae8006887210e6c2f8583bcee5f489
faineance <faineance@users.noreply.github.com>
2016-01-07 23:47:07 +0000

    GADTs and optimize

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE Arrows    #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs     #-}
 module Main where
 
 import           Control.Arrow
@@ -22,12 +23,22 @@ data Instr = Push Register
         deriving Show
 
 -- Input lang
-data Expr = Lit Int
-            | Expr :+: Expr
-            | Expr :*: Expr
-          deriving Show
 
-data Stmt = Seq [Stmt]
+-- data Expr = Lit Int
+--             | Expr :+: Expr
+--             | Expr :*: Expr
+--           deriving Show
+
+data Expr t where
+    Lit :: Int -> Expr Int
+    (:+:) :: Expr Int -> Expr Int -> Expr Int
+    (:*:) :: Expr Int -> Expr Int -> Expr Int
+
+instance Show (Expr t) where
+    show (Lit i) = "Lit " ++ show i
+    show ((:+:) e e') = show e ++ " + " ++ show e'
+    show ((:*:) e e') = show e ++ " + " ++ show e'
+
 -- Yay arrows
 type X86_64 a b = WriterArrow [Instr] (Kleisli Identity) a b
 
@@ -57,7 +68,7 @@ add target param = output $ Add target param
 mul :: Register -> Register -> X86_64 n n
 mul target param = output $ Mul target param
 
-compile :: Expr -> X86_64 n (Succ n)
+compile :: Expr t -> X86_64 n (Succ n)
 compile (Lit n) = set RAX n >>>
                     push RAX
 
@@ -70,11 +81,21 @@ compile (e :*: e') = compile e >>>
                      op mul
 
 assemble :: X86_64 n (Succ n) -> [Instr]
-assemble program = snd $ runIdentity (runKleisli (elimWriter program) initial)
+assemble program = snd $ runIdentity $ runKleisli (elimWriter program) initial
     where initial = undefined :: n
 
+
+-- Cleanup needed ( maybe fixUntil :: (a -> a -> Bool) -> (a -> a) -> a -> a )
+optimize :: [Instr] -> [Instr]
+optimize [] = []
+optimize (Push r : Pop r' : xs) | r == r' = optimize xs
+optimize (Push x : Push y : Pop y' : Pop x': xs) | x == x' && y == y' = optimize xs
+optimize (Push x : Push y : Push z : Pop z' : Pop y' : Pop x': xs) | x == x' && y == y' && z == z' = optimize xs
+optimize (Set v r : Set v' r' : xs) | r == r' && v == v' = optimize xs
+optimize (x : xs) = x : optimize xs
+
 test :: [Instr]
-test = assemble (compile (Lit 10 :+: Lit 5 :*: Lit 3))
+test = optimize $ assemble $ compile (Lit 10 :+: Lit 5 :*: Lit 3)
 
 main :: IO ()
 main = print test
