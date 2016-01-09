ffac90b13b2af904adff4f157711b50e919cf0e7
faineance <faineance@users.noreply.github.com>
2016-01-09 21:40:03 +0000

    remove commented out code

--- src/Optimization.hs
+++ src/Optimization.hs
@@ -21,17 +21,3 @@ instance Optimisable [Instr] where
     optimize (Push r : Pop r' : xs) | r == r' = xs
     optimize (Set r v : x'@(Set r' v') : xs ) = x' : xs
     optimize (x : xs) = x : optimize xs
-
-
-
--- type Optimization = Program -> Writer [String] Program
-
--- optimize :: Program
--- optimize program = fst $ runWriter $ foldM (\program f -> f program) program [ reduce ]
---
---
---
--- reduce :: Optimization
--- reduce (Push r : Pop r' : xs) | r == r' = return xs
--- reduce (Set r v : x'@(Set r' v') : xs ) = return (x' : xs)
--- reduce program = return program
